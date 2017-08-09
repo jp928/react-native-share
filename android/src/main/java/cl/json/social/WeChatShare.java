12a656095778c517d91a3feef454aeaa299a231e
piaojingtai <piaojingtai@gmail.com>
2017-08-09 17:19:04 +1000

    Add share via linkedin and wechat

---
+++ android/src/main/java/cl/json/social/WeChatShare.java
@@ -0,0 +1,40 @@
+package cl.json.social;
+
+import android.content.ActivityNotFoundException;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReadableMap;
+
+/**
+ * Created by piaojingtai on 9/8/17.
+ */
+
+public class WeChatShare extends SingleShareIntent {
+
+    private static final String PACKAGE = "com.tencent.mm";
+    private static final String PLAY_STORE_LINK = "market://details?id=com.tencent.mm";
+
+    public WeChatShare(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public void open(ReadableMap options) throws ActivityNotFoundException {
+        super.open(options);
+        //  extra params here
+        this.openIntentChooser();
+    }
+    @Override
+    protected String getPackage() {
+        return PACKAGE;
+    }
+
+    @Override
+    protected String getDefaultWebLink() {
+        return null;
+    }
+
+    @Override
+    protected String getPlayStoreLink() {
+        return PLAY_STORE_LINK;
+    }
+}
